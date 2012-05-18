727974351eb45993620af1cbeb58140067101d41
Christian Mikula <7902396+cmikula@users.noreply.github.com>
2012-05-18 16:42:58 +0000

    First check for ISO9660 format (we don't need to mount all iso files)

--- src/MoviePlayer.py
+++ src/MoviePlayer.py
@@ -359,12 +359,13 @@ def movieSelected(self, service):
         if isinstance(service, eServiceReferenceDvd) and service.getPath().endswith(".iso"):
             from ServiceProvider import ISOInfo
             iso = ISOInfo()
-            iso_format = iso.getFormat(service)
-            if iso_format == ISOInfo.UNKNOWN:
-                self.session.open(MessageBox, _("Selected iso file is not playable!"), MessageBox.TYPE_ERROR)
-                return
-            if iso_format == ISOInfo.BLUDISC:
-                service = iso.getService(service)
+            if iso.getFormatISO9660(service) != ISOInfo.DVD:
+                iso_format = iso.getFormat(service)
+                if iso_format == ISOInfo.UNKNOWN:
+                    self.session.open(MessageBox, _("Selected iso file is not playable!"), MessageBox.TYPE_ERROR)
+                    return
+                if iso_format == ISOInfo.BLUDISC:
+                    service = iso.getService(service)
         if isinstance(service, eServiceReferenceDvd):
             if pluginPresent.DVDPlayer:
                 from Plugins.Extensions.DVDPlayer.plugin import DVDPlayer as eDVDPlayer
