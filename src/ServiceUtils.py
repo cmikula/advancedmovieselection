52a1d167282cb09b90b989c61aa612175facf149
Christian Mikula <7902396+cmikula@users.noreply.github.com>
2012-09-01 15:07:55 +0000

    Add disk usage

--- src/ServiceUtils.py
+++ src/ServiceUtils.py
@@ -391,6 +391,18 @@ class eServiceReference():
     def getShortDescription(self):
         return self.short_description
 
+def disk_usage(path):
+    """Return disk usage statistics about the given path.
+
+    Returned valus is a named tuple with attributes 'total', 'used' and
+    'free', which are the amount of total, used and free space, in bytes.
+    """
+    st = os.statvfs(path)
+    free = st.f_bavail * st.f_frsize
+    total = st.f_blocks * st.f_frsize
+    used = (st.f_blocks - st.f_bfree) * st.f_frsize
+    return total, used, free
+
 def realSize(bytes, digits=1, factor=1024):
     size = float(bytes)
     s = "%%.%df" % (digits)
