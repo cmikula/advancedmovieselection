a9b99c3fdd0aee6e9264174e1f7100719fa5228b
Christian Mikula <7902396+cmikula@users.noreply.github.com>
2015-12-04 15:04:29 +0000

    Fix crash on set movie tags.

--- src/TagEditor.py
+++ src/TagEditor.py
@@ -136,7 +136,7 @@ class TagEditor(Screen):
         else:
             file = file + ".ts.meta"
         if os_path.exists(file):
-            metafile = open(file + ".ts.meta", "r")
+            metafile = open(file, "r")
             sid = metafile.readline().strip("\r\n")
             title = metafile.readline().strip("\r\n")
             descr = metafile.readline().strip("\r\n")
@@ -146,7 +146,7 @@ class TagEditor(Screen):
             metafile.close()
             tags = " ".join(tags)
             if tags != oldtags:
-                metafile = open(file + ".ts.meta", "w")
+                metafile = open(file, "w")
                 metafile.write("%s\n%s\n%s\n%s\n%s\n%s" % (sid, title, descr, time, tags, rest))
                 metafile.close()
 
