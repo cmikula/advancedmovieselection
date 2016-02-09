fd632c66eeb07d9415179224efa980e8b18d45e3
Christian Mikula <7902396+cmikula@users.noreply.github.com>
2016-02-09 16:16:03 +0000

    Remove branch from build

--- build/make-all-win.bat
+++ build/make-all-win.bat
@@ -1,16 +1,17 @@
 @echo off
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python26\python.exe" -O build.py -sbranches/all_oe -tipk
+"%ProgramFiles(x86)%\Python26\python.exe" -O build.py -tipk
 rem echo BUILD returned %ERRORLEVEL%!
 If not %errorlevel% == 0 goto error
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/all_oe -tipk
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -tipk
 If not %errorlevel% == 0 goto error
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/all_oe -tdeb
+rem "%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/all_oe -tdeb
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -tdeb
 If not %errorlevel% == 0 goto error
 
 goto success
