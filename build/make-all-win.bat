9c0668ca19c5ce9c4ee41f547042041c04603789
cmikula <7902396+cmikula@users.noreply.github.com>
2016-05-21 09:09:14 +0000

    Skin changes from zombi Change version to unstable

--- build/make-all-win.bat
+++ build/make-all-win.bat
@@ -1,17 +1,7 @@
 @echo off
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python26\python.exe" -O build.py -tipk
-rem echo BUILD returned %ERRORLEVEL%!
-If not %errorlevel% == 0 goto error
-
-if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -tipk
-If not %errorlevel% == 0 goto error
-
-if exist .\deploy\build del .\deploy\build\CONTROL\control
-rem "%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/all_oe -tdeb
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -tdeb
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/oe22_unstable -tdeb
 If not %errorlevel% == 0 goto error
 
 goto success
--- src/Version.py
+++ src/Version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = "4.1"
+__version__ = "4.1_unstable"
 __date__ = "?"
 __branch__ = "?"
 __revision__ = "?"
