c111059221983464d2f3065bfcd40e6481da6ed3
cmikula <7902396+cmikula@users.noreply.github.com>
2018-02-11 15:27:02 +0000

    Change build repo path

--- build/make-all-win.bat
+++ build/make-all-win.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/unstable_v6 -tdeb
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/oe25_unstable -tdeb
 If not %errorlevel% == 0 goto error
 
 goto success
