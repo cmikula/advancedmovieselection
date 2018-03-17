e51f3ee6741d97b105a5a5a2494ea3e95732c6a6
cmikula <7902396+cmikula@users.noreply.github.com>
2018-03-17 18:40:41 +0000

    Merged revision(s) 1661 from trunk: Change build for arch all. ........

--- build/make-all-win.bat
+++ build/make-all-win.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/oe22_unstable -tdeb
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -t"deb" -a"all" -s"branches/oe22_unstable"
 If not %errorlevel% == 0 goto error
 
 goto success
