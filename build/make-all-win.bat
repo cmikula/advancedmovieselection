18170d54289a10f5109a83ba2e5a1a30dbf3f699
cmikula <7902396+cmikula@users.noreply.github.com>
2018-03-17 18:46:29 +0000

    Merged revision(s) 1661 from trunk: Change build for arch all. ........

--- build/make-all-win.bat
+++ build/make-all-win.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/oe25_unstable -tdeb
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -t"deb" -a"all" -s"branches/oe25_unstable" -d"enigma2 (>= 4.3.1r24)"
 If not %errorlevel% == 0 goto error
 
 goto success
