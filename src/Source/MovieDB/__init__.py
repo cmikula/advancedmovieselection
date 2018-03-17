8df4045b3504581a53627000e9be04d0c5e7b2d1
cmikula <7902396+cmikula@users.noreply.github.com>
2018-03-17 17:43:57 +0000

    Merged revision(s) 1654-1657 from trunk: Add moviedb debug messages. Add missing connection timeouts. ........ Timeout for download cover. ........ Add help window pixmap. ........ Add help window pixmap. ........

--- src/Source/MovieDB/__init__.py
+++ src/Source/MovieDB/__init__.py
@@ -1,4 +1,14 @@
-import os, urllib
+import os
+from TMDb.tmdbapi import CONNECTION_TIMEOUT
+
+REQUESTS = False
+try:
+    import requests #@UnresolvedImport
+    REQUESTS = True
+except:
+    import json
+    import urllib2
+
 #from ..StopWatch import clockit
 
 #@clockit
@@ -8,7 +18,17 @@ def downloadCover(url, filename, overwrite=False):
             return False
         if (not os.path.exists(filename) or overwrite) and url:
             #print "Try loading: ", str(url), "->", str(filename)
-            urllib.urlretrieve(url, filename)
+            if REQUESTS:
+                print "request", str(url)
+                r = requests.get(url, timeout=CONNECTION_TIMEOUT, stream=True)
+                with open(filename, 'wb') as f:
+                    f.write(r.content)
+            else:
+                print "urlopen", str(url)
+                req = urllib2.Request(url=url)
+                r = urllib2.urlopen(req, timeout=CONNECTION_TIMEOUT)
+                with open(filename, 'wb') as f:
+                    f.write(r.read())
         else:
             pass
             #print "Download skipped:", str(url), "->", str(filename)
