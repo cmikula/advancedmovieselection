9dafe985adc687637c46dfcb2da20930abf6e3ef
cmikula <7902396+cmikula@users.noreply.github.com>
2018-03-17 19:01:21 +0000

    Reorder build packages

--- build/make-all-win.bat
+++ build/make-all-win.bat
@@ -1,12 +1,11 @@
 @echo off
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-rem "%ProgramFiles(x86)%\Python26\python.exe" -O build.py -tipk
-rem echo BUILD returned %ERRORLEVEL%!
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -t"ipk" -a"all"
 If not %errorlevel% == 0 goto error
 
 if exist .\deploy\build del .\deploy\build\CONTROL\control
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -t"ipk" -a"all"
+"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -t"deb" -a"all"
 If not %errorlevel% == 0 goto error
 
 rem build oe22
@@ -20,10 +19,6 @@ if exist .\deploy\build del .\deploy\build\CONTROL\control
 If not %errorlevel% == 0 goto error
 
 
-if exist .\deploy\build del .\deploy\build\CONTROL\control
-rem "%ProgramFiles(x86)%\Python27\python.exe" -O build.py -sbranches/all_oe -tdeb
-"%ProgramFiles(x86)%\Python27\python.exe" -O build.py -t"deb" -a"all"
-If not %errorlevel% == 0 goto error
 
 goto success
 
